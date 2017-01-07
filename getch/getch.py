7e9cc7f7dd618b535f8deea5acac24b733a2a615
elias <elias@bytekite.io>
2017-01-07 05:55:54 +0000

    revert control sequences inclusion

--- getch/getch.py
+++ getch/getch.py
@@ -11,7 +11,7 @@ except ImportError:
         fd = sys.stdin.fileno()
         old = termios.tcgetattr(fd)
         try:
-            tty.setcbreak(fd)
+            tty.setraw(fd)
             return sys.stdin.read(1)
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old)
